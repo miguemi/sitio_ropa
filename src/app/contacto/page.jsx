48cedf574d908e737e445782f92d50c7723030a8
alanfvn <alandavidg16@gmail.com>
2023-07-25 16:06:20 -0600

    creacion de pagina de contacto

---
+++ src/app/contacto/page.jsx
@@ -0,0 +1,16 @@
+import CustomNavbar from "../components/header";
+import CustomFooter from "../components/footer";
+
+export default function Catalogo(){
+  return (
+    <div className="layout">
+      <CustomNavbar/>
+      <main>
+        <div className="container">
+          <h1>Contacto</h1>
+        </div>
+      </main>
+      <CustomFooter/>
+    </div>
+  )
+}
